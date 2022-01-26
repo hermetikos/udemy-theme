fe1fb480b8355f22a451326d53c9347fccd30883
Stan S <sslup@hermetikos.com>
2022-01-25 23:49:40 -0500

    Finishes basic enqueueing and adds debug flag

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 // Setup
+define( 'JU_DEV_MODE', true);
 
 // Includes
 include( get_theme_file_path( '/includes/front/enqueue.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
-// Shortcodes
+
+
+?>
--- index.php
+++ index.php
@@ -544,10 +544,7 @@
 
   <!-- External JavaScripts
   ============================================= -->
-  <script src="js/jquery.js"></script>
-  <script src="js/plugins.js"></script>
-  <script src="js/functions.js"></script>
-
+  <?php wp_footer() ?>
 </body>
 
 </html>
