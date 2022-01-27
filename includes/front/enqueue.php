9594f2b601c09d9480be27c54f336e8f8ed72c7b
Stan S <sslup@hermetikos.com>
2022-01-27 12:19:30 -0500

    Implements main menu in template

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -3,7 +3,11 @@ function ju_enqueue() {
     $uri = get_theme_file_uri();
     $ver = JU_DEV_MODE ? time() : false;
 
-    wp_register_style('ju_google_fonts', "https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i", $ver);
+    wp_register_style(
+        'ju_google_fonts',
+        "https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i",
+        $ver
+    );
     wp_register_style('ju_bootstrap', $uri . '/assets/css/bootstrap.css', $ver);
     wp_register_style('ju_style', $uri . '/assets/css/style.css', $ver);
     wp_register_style('ju_dark', $uri . '/assets/css/dark.css', $ver);
