e1ea471e6073bbf466cb8bb3fb1f52aad50855f1
Stan S <sslup@hermetikos.com>
2022-01-27 20:15:55 -0500

    Adds sidebar to template

---
+++ includes/widgets.php
@@ -0,0 +1,15 @@
+<?php
+
+function ju_widgets() {
+    register_sidebar([
+        'name' => __( 'My First Sidebar', 'udemy theme'),
+        'id' => 'ju_sidebar',
+        'description' => __( 'Sidebar for Udemy theme', 'udemy theme'),
+        'before_widget' => '<div id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>'
+    ]);
+}
+
+?>
