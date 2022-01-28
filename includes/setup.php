ae46e3109469c3ec0d771011a1705c6f00ef992a
Stan S <sslup@hermetikos.com>
2022-01-27 20:20:46 -0500

    Changes namespace for translation

--- includes/setup.php
+++ includes/setup.php
@@ -1,7 +1,7 @@
 <?php
 
 function ju_setup_theme() {
-    register_nav_menu( 'primary', __( 'Primary Menu', 'udemy theme') );
+    register_nav_menu( 'primary', __( 'Primary Menu', 'udemy') );
 }
 
 ?>
--- includes/widgets.php
+++ includes/widgets.php
@@ -4,12 +4,15 @@ function ju_widgets() {
     register_sidebar([
         'name' => __( 'My First Sidebar', 'udemy theme'),
         'id' => 'ju_sidebar',
-        'description' => __( 'Sidebar for Udemy theme', 'udemy theme'),
+        'description' => __( 'Sidebar for Udemy theme', 'udemy'),
         'before_widget' => '<div id="%1$s" class="widget clearfix %2$s">',
         'after_widget' => '</div>',
         'before_title' => '<h4>',
         'after_title' => '</h4>'
     ]);
+    /*
+    register a sidebar
+    */
 }
 
 ?>
