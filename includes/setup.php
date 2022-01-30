d74c5a3281982b6e3a5543e2e715eb357f1c8422
MisturDust319 <jarlofearls@gmail.com>
2022-01-30 17:35:11 -0500

    adds basic Loop support

--- includes/setup.php
+++ includes/setup.php
@@ -1,6 +1,10 @@
 <?php
 
 function ju_setup_theme() {
+    add_theme_support( 'post-thumbnails' );
+    /*
+    enables featured images for your posts
+    */
     register_nav_menu( 'primary', __( 'Primary Menu', 'udemy') );
 }
 
