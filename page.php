06cec654534356939a7457b6475c9cf9dad7c0cd
Stan S <sslup@hermetikos.com>
2022-02-12 00:54:34 -0500

    Implements basic page template

---
+++ page.php
@@ -0,0 +1,144 @@
+<?php
+
+get_header();
+
+while( have_posts() )
+{
+    the_post();
+    
+    ?>
+
+    <!-- Page Title
+    ============================================= -->
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1><?php the_title(); ?></h1>
+            <span>
+                <?php
+                    do_action( 'plugins/wp_subtitle/the_subtitle', array(
+                        'before'        => '<p class="subtitle">',
+                        'after'         => '</p>',
+                        'post_id'       => get_the_ID(),
+                        'default_value' => ''
+                    ));
+                ?>
+            </span>
+        </div>
+    </section><!-- #page-title end -->
+    
+    <?php
+}
+
+rewind_posts();
+?>
+
+
+
+<!-- Content
+============================================= -->
+<section id="content">
+
+    <div class="content-wrap">
+
+        <div class="container clearfix">
+
+            <!-- Post Content
+      ============================================= -->
+            <div class="postcontent nobottommargin clearfix">
+                <?php
+
+                while (have_posts()) {
+                    the_post();
+
+                    global $post;
+                    // this is a built in WP variable that contains post info
+                    // like the author and the author URL, which we'll use repeatedly in this template
+                    $author_ID = $post->post_author;
+                    $author_URL = get_author_posts_url( $author_ID );
+
+
+                    ?>
+
+                    <div class="single-post nobottommargin">
+
+                        <!-- Single Post
+                        ========================================= -->
+                        <div class="entry clearfix">
+
+                            
+
+                            <!-- Entry Image
+                            ============================================= -->
+                            <div class="entry-image">
+                                <?php
+                                    if(has_post_thumbnail())
+                                        {  
+                                            ?>
+                                            <div class="entry-image">
+                                                <a href="<?php the_permalink(); ?>">
+                                                    <?php the_post_thumbnail(
+                                                        'full',
+                                                        [ 'class' => 'image_fade' ]
+                                                    ); ?>
+                                                </a>
+                                            </div>
+                                            <?php
+                                        }
+                                    ?>
+                            </div><!-- .entry-image end -->
+
+                            <!-- Entry Content
+                            ============================================= -->
+                            <div class="entry-content notopmargin">
+                                
+                                <?php the_content();
+                                    $defaults = array(
+                                        'before'           => '<p class="text-center">' . __( 'Pages:' ),
+                                        'after'            => '</p>'
+                                    );
+
+                                    wp_link_pages($defaults);
+                                    // use link pages to enable post pagination
+                                ?>
+                                
+                                <!-- Post Single - Content End -->
+
+                                <div class="clear"></div>
+
+                        </div>
+
+                    
+                        <div class="line"></div>
+
+                        <!-- Comments
+                        ============================================= -->
+                        <?php
+                            if( comments_open() || get_comments_number()) {
+                                // comments_open() returns true if the post is open for comments
+                                // however, we always want to display comments if there are some, even if the page
+                                // has been closed to new comments
+                                comments_template();
+                            }
+                        ?>
+                        <!-- #comments end -->
+
+                    </div>
+                        </div>
+                <?php
+                    }
+                ?>
+
+            </div><!-- .postcontent end -->
+
+            <!-- Sidebar
+            ============================================= -->
+            <?php get_sidebar() ?>
+            <!-- .sidebar end -->
+
+        </div>
+
+    </div>
+
+</section><!-- #content end -->
+
+<?php get_footer(); ?>
