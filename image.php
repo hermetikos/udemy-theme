6ba2cfcb4c12a62ba15438f716b6d62f6cc1ef5d
Stan S <sslup@hermetikos.com>
2022-02-16 17:48:00 -0500

    Adds attachment and image templates

---
+++ image.php
@@ -0,0 +1,92 @@
+
+<?php get_header(); ?>
+
+<!-- Content
+============================================= -->
+<section id="content">
+
+    <div class="content-wrap">
+
+        <div class="container clearfix">
+
+            <!-- Post Content
+      ============================================= -->
+            <div class="postcontent nobottommargin clearfix">
+                <?php
+
+                if( have_posts() ) {
+                    while (have_posts()) {
+                        the_post();
+
+                        ?>
+
+                        <div class="single-post nobottommargin">
+
+                        <!-- Single Post
+                        ========================================= -->
+                        <div class="entry clearfix">
+                            <!-- Entry Content
+                            ============================================= -->
+                            <div class="entry-content notopmargin">
+                            <a href="<?php echo $post->guid; ?>">
+                                Direct Download
+                            </a>
+                            <img src="<?php echo $post->guid; ?>" class="img-responsive" />
+                            <?php
+
+                            the_content();
+
+                            ?>
+
+                            </div>
+                        </div><!-- .entry end -->
+
+                        <div class="line"></div>
+
+                        <!-- Comments
+                        ============================================= -->
+                        <?php
+                            if( comments_open() || get_comments_number()) {
+                                // comments_open() returns true if the post is open for comments
+                                // however, we always want to display comments if there are some, even if the page
+                                // has been closed to new comments
+                                comments_template();
+                            }
+                        ?>
+                        <!-- #comments end -->
+
+                    </div>
+            </div>
+                <?php
+                    }
+                }
+                ?>
+
+            </div><!-- .postcontent end -->
+
+            <!-- Sidebar
+            ============================================= -->
+            <?php get_sidebar() ?>
+            <!-- .sidebar end -->
+
+        </div>
+
+    </div>
+
+</section><!-- #content end -->
+
+<?php get_footer(); ?>
+
+<!-- NOTES
+    mime types
+        a standard for identify content types (html, php, pdf, and so on)
+        browsers use these to identify content types
+        it's more reliable than a file extension, as it is stored in the file itself
+        it isn't foolproof
+    MIME and WP
+        You can create templates for specific types of MIME types
+            like this image.php template
+            Mime specific templates have the highest priority of all attachment templates
+        WP checks the mime type of a file        
+    
+ -->
