a03eec29e7bbd9470b4a85d5c85ac69318c2a812
Stan S <sslup@hermetikos.com>
2022-03-25 00:17:38 -0400

    Adds misc section to customizer

--- footer.php
+++ footer.php
@@ -9,9 +9,16 @@
         <div class="container clearfix">
 
           <div class="col_half">
-            Copyrights &copy; 2017 All Rights Reserved by Udemy.<br>
+            <?php echo get_theme_mod('ju_footer_copyright_text'); ?><br>
             <div class="copyright-links">
-              <a href="#">Privacy Policy</a>
+              <?php
+                if( get_theme_mod( 'ju_footer_tos_page')) {
+                  ?><a href="<?php the_permalink(get_theme_mod('ju_footer_tos_page')); ?>">Terms of Use</a><?php
+                }
+                if( get_theme_mod( 'ju_footer_privacy_page')) {
+                  ?><a href="<?php the_permalink(get_theme_mod('ju_footer_privacy_page')); ?>">Privacy Policy</a><?php
+                }
+              ?>
             </div>
           </div>
 
